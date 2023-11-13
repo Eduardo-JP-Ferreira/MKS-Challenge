c3641a86e2f1540cff88b3a5187cd83d6f468c3d
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-11-13 19:16:10 -0300

    Initial Cleaning and Preparation

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import ResetStyle from "./style/ResetStyle.ts";
+import GlobalStyle from "./style/GlobalStyle.ts";
+import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <ResetStyle />
+    <GlobalStyle />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
