7a2548cd3bccab17bf383f7eddd72d79cb9a2f87
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-11-24 18:19:55 -0300

    Add Product Model Prototype

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,15 +1,68 @@
 import styled from "styled-components";
+import logo from "../assets/logo.png";
 
 export default function Products() {
-  return <ContainerProducts></ContainerProducts>;
+  return (
+    <ContainerProducts>
+      <ContainerItem>
+        <img src={logo} alt="Product Image"></img>
+        <h1>Product Name</h1>
+        <Price>
+          <h1>R$20,00</h1>
+        </Price>
+      </ContainerItem>
+
+      <ContainerItem>
+        <img src={logo} alt="Product Image"></img>
+        <h1>Product Name</h1>
+        <Price>
+          <h1>R$20,00</h1>
+        </Price>
+      </ContainerItem>
+    </ContainerProducts>
+  );
 }
 
 const ContainerProducts = styled.div`
   background-color: lightcoral;
-  width: 100%;
-  height: 100px;
+  width: 80%;
+  height: 500px;
   display: flex;
-  justify-content: space-between;
   flex-wrap: wrap;
   align-items: center;
+  gap: 20px;
+`;
+
+const ContainerItem = styled.div`
+  background-color: lightblue;
+  width: 200px;
+  height: 250px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  img {
+    width: 150px;
+    height: 150px;
+    border-radius: 5%;
+    margin-top: 10px;
+  }
+  h1 {
+    font-size: 23px;
+    color: black;
+    font-weight: bold;
+  }
+`;
+
+const Price = styled.div`
+  width: 100px;
+  height: 40px;
+  background-color: blue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 10%;
+  h1 {
+    color: white;
+  }
 `;
