ce1f17a3a6df580e16a13f0fa1cee1c316bddacb
Eduardo-JP-Ferreira <eduardopires2014@gmail.com>
2023-11-28 17:35:52 -0300

    Fix: Product Name Overflow Problem

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -6,17 +6,17 @@ export default function Products() {
     <ContainerProducts>
       <ContainerItem>
         <img src={logo} alt="Product Image"></img>
-        <h1>Product Name</h1>
+        <h1>sdasdasdasdasdasda sdasd asdaSAsA SASsdadasd</h1>
         <Price>
-          <h1>R$20,00</h1>
+          <h2>R$20,00</h2>
         </Price>
       </ContainerItem>
 
       <ContainerItem>
         <img src={logo} alt="Product Image"></img>
-        <h1>Product Name</h1>
+        <h1>Product Name asdasdasdasdas dasdasd asd asda asdasdasd</h1>
         <Price>
-          <h1>R$20,00</h1>
+          <h2>R$2000,00</h2>
         </Price>
       </ContainerItem>
     </ContainerProducts>
@@ -36,21 +36,32 @@ const ContainerProducts = styled.div`
 const ContainerItem = styled.div`
   background-color: lightblue;
   width: 200px;
-  height: 250px;
+  height: 260px;
+  max-width: 200px;
+  max-height: 260px;
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 10px;
+  overflow: hidden;
+  text-overflow: ellipsis;
   img {
-    width: 150px;
-    height: 150px;
+    width: 180px;
+    height: 130px;
     border-radius: 5%;
     margin-top: 10px;
   }
   h1 {
-    font-size: 23px;
+    font-size: 20px;
     color: black;
-    font-weight: bold;
+    max-width: 90%;
+    max-height: 50px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    display: -webkit-box;
+    -webkit-line-clamp: 2;
+    -webkit-box-orient: vertical;
+    white-space: normal;
   }
 `;
 
@@ -62,7 +73,9 @@ const Price = styled.div`
   justify-content: center;
   align-items: center;
   border-radius: 10%;
-  h1 {
+
+  h2 {
     color: white;
+    font-size: 1vw;
   }
 `;
